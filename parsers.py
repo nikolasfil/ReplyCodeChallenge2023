e947dac4a9ed1ee023e2fb6a84d1e9c7c8d70e9b
HliasOuzounis <iliouzoun@outlook.com>
2023-03-09 17:48:22 +0200

    parser complete

--- parsers.py
+++ parsers.py
@@ -11,7 +11,20 @@ def save_data(file,data):
 
 
 def main():
-    pass
+    file_name = "inputs/00-example.txt"
+    
+    data = get_data(file_name)
+    R, C, S = tuple(map(int, data[0].split()))
+    snake_lenghts = list(map(int, data[1].split()))
+    matrix = [row.split() for row in data[2:]]
+    for r, row in enumerate(matrix):
+        for c, value in enumerate(row):
+            if value.isdigit():
+                matrix[r][c] = int(value)
+    
+    
+    return R, C, S, snake_lenghts, matrix
+    
 
 
 if __name__ == '__main__':
